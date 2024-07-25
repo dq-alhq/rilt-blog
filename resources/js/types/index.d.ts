065162b523d0a8be6edf2e2f6fce5175f8e0da54
Diqi Al-Haqqi <dq.alhaqqi@gmail.com>
2024-07-25 09:10:21 +0700

    --wip--

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,5 +1,3 @@
-import { Config } from 'ziggy-js'
-
 export interface User {
     id: number
     name: string
@@ -13,5 +11,4 @@ export type PageProps<T extends Record<string, unknown> = Record<string, unknown
     auth: {
         user: User
     }
-    ziggy: Config & { location: string }
 }

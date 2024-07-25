b139e79445f23da983870b8042364c4291f622ff
Diqi Al-Haqqi <dq.alhaqqi@gmail.com>
2024-07-25 09:04:30 +0700

    --wip--

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import react from '@vitejs/plugin-react'
+import laravel from 'laravel-vite-plugin'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: "resources/js/app.tsx",
-            ssr: "resources/js/ssr.tsx",
-            refresh: true,
+            input: 'resources/js/app.tsx',
+            ssr: 'resources/js/ssr.tsx',
+            refresh: true
         }),
-        react(),
+        react()
     ],
     resolve: {
         alias: {
-            ui: resolve("@/components/ui"),
-        },
-    },
-});
+            ui: resolve('@/components/ui'),
+            'ziggy-js': resolve('vendor/tightenco/ziggy')
+        }
+    }
+})
